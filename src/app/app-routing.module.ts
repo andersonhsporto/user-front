bbb3afb95d260c33736860a79f4727985fccfd74
Anderson Porto <ahigo@minsait.com>
2023-10-17 15:14:53 -0300

    Add: add user component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './feature/user/component/add-user/add-user.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     redirectTo: 'home',
+  },
+  {
+    path: 'home', component: AddUserComponent
   },
 ];
 
